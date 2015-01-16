e3718cdec66fb3e6ed96cb6f95283f4885a1e214
Toru, Kobayashi <koba0004@gmail.com>
2015-01-17 00:42:42 +0900

    Change listen port

--- config/production.js
+++ config/production.js
@@ -3,7 +3,7 @@
 module.exports = {
   debug: false, //useful for seeing some logs in the browser console
   siteUrl: 'koba04-english-blog.herokuapp.com', // the url of your website
-  port: 80,
+  port: process.env.PORT,
   ip: '0.0.0.0',
   googleAnalytics : '' //your google analytics tracking code
 };
