ad48728e85f9356d1e2a3047927c7153ec6322e9
Toru, Kobayashi <koba0004@gmail.com>
2015-01-17 00:45:23 +0900

    Change siteUrl to FQDN

--- config/production.js
+++ config/production.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   debug: false, //useful for seeing some logs in the browser console
-  siteUrl: 'koba04-english-blog.herokuapp.com', // the url of your website
+  siteUrl: 'http://koba04-english-blog.herokuapp.com', // the url of your website
   port: process.env.PORT,
   ip: '0.0.0.0',
   googleAnalytics : '' //your google analytics tracking code
