a9602823c547d5f15f0093101c056ddc1a89c9fd
Toru Kobayashi <koba0004@gmail.com>
2015-02-04 19:31:16 +0900

    Add highlight.js load for initial load

--- content/themes/koba04/Html.js
+++ content/themes/koba04/Html.js
@@ -43,6 +43,7 @@ var Html = React.createClass({
       <body dangerouslySetInnerHTML={{__html: this.props.markup}}></body>
     <script dangerouslySetInnerHTML={{__html: this.props.state}}></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.4/highlight.min.js"></script>
+    <script>hljs.initHighlightingOnLoad();</script>
     <script src="/assets/dist/bundle-61bd0948.min.js"></script>
     </html>
     )
